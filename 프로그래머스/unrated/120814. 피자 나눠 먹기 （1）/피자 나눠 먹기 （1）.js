afe7c0d72f17fd9532d290f0139274c261157156
Soohyun Jung <78977003+sasha1107@users.noreply.github.com>
2022-10-12 20:53:06 +0900

    [unrated] Title: 피자 나눠 먹기 (1), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 0;
+    if (n <= 7){
+        return 1;
+    }
+    if (n % 7 == 0){
+       answer =  Math.floor(n / 7);
+    }
+    else {
+        answer = Math.floor(n / 7) + 1;    
+    }
+    return answer;
+}
