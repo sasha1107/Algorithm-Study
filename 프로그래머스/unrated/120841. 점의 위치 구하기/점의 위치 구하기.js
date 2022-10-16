7cbb92e77f8a882cb298a31cc1498a6ac0b11ddd
Soohyun Jung <78977003+sasha1107@users.noreply.github.com>
2022-10-16 16:56:39 +0900

    [unrated] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,10 @@
+function solution(dot) {
+    var answer = 0;
+    if (dot[0] < 0){
+        answer = dot[1] > 0 ? 2: 3;
+    }
+    else if (dot[0] > 0){
+        answer = dot[1] > 0 ? 1: 4;
+    }
+    return answer;
+}
