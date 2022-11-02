cb5e9902f65bf761d2304746b8bf33cbe029d240
Soohyun Jung <78977003+sasha1107@users.noreply.github.com>
2022-11-02 11:51:24 +0900

    [level 2] Title: 오픈채팅방, Time: 249.72 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,25 @@
+
+
+function solution(record) {
+    let users = {}
+    let result = [];
+    for (const i of record){
+        const [state, id, nickname] = i.split(' ');
+        if (state === "Enter"){
+            users[id] = nickname;
+        } else if (state === "Change"){
+            users[id] = nickname;
+        }
+    }
+    for (const i of record){
+        const [state, id, nickname] = i.split(' ');
+        if (state === "Enter"){
+            result.push(`${users[id]}님이 들어왔습니다.`);
+        } else if (state === "Leave"){
+            result.push(`${users[id]}님이 나갔습니다.`);
+        }
+    }
+    return result;
+    
+}
+
