a7797e353f6bee82e0278a98cab9bf535cf45072
Soohyun Jung <78977003+sasha1107@users.noreply.github.com>
2022-11-04 17:53:57 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,5 +1,6 @@
 function solution(dots) {
-    let x = Math.max(dots[0][0], dots[1][0], dots[2][0], dots[3][0]) - Math.min(dots[0][0], dots[1][0], dots[2][0], dots[3][0])
-    let y = Math.max(dots[0][1], dots[1][1], dots[2][1], dots[3][1]) - Math.min(dots[0][1], dots[1][1], dots[2][1], dots[3][1])
-    return x * y
+    const x = dots.map(n => n[0]);
+    const y = dots.map(n => n[1]);
+
+    return (Math.max(...x) - Math.min(...x)) * (Math.max(...y) - Math.min(...y));
 }
