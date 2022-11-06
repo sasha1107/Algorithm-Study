1a90fd6fa0d90ff6718c4fe5edd4fb09f796f508
Soohyun Jung <78977003+sasha1107@users.noreply.github.com>
2022-11-06 20:32:25 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,3 +1,3 @@
 function solution(n) {
-    return n % 2 == 0 ? Array(~~(n/2)).fill(0).map((_, index) => index * 2 + 1) : Array(~~(n/2)+1).fill(0).map((_, index) => index * 2 + 1);
+    return Array(n).fill(0).map((_, index) => index + 1).filter(i => i % 2 == 1)
 }
