b2a1cdacabd45a9f46080b15e6b23a6aff2018ef
Soohyun Jung <78977003+sasha1107@users.noreply.github.com>
2022-11-06 21:30:10 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,8 +1,3 @@
 function solution(my_string) {
-    let answer = [];
-    my_string.split("").filter(i => i == 0 || Number(i)).sort((a,b) => a-b).forEach(i =>     {
-        answer.push(parseInt(i))
-    });
-    console.log(answer);
-    return answer
+    return my_string.replaceAll(/[^0-9]/g, '').split('').sort((a,b) => a-b).map(v => +v)
 }
