834617fa3f6d3cbe9ec13974773ec809d19760ef
Soohyun Jung <78977003+sasha1107@users.noreply.github.com>
2022-11-06 20:50:12 +0900

    [level 0] Title: 약수 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,9 +1,3 @@
 function solution(n) {
-    let result = [];
-    for (let i=1; i<n+1; i++){
-        if (n % i == 0){
-            result.push(i);
-        }
-    }
-    return result;
+    return Array(n).fill().map((_, i) => i + 1).filter(i => n % i === 0)
 }
