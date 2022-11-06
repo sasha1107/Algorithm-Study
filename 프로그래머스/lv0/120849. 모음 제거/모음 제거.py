8805431570e41cb23a39c5b482d3946d7fcdac60
Soohyun Jung <78977003+sasha1107@users.noreply.github.com>
2022-11-06 21:44:20 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -1,2 +1,4 @@
+import re
+
 def solution(my_string):
-    return my_string.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')
+    return re.sub(r'[aeiou]', '', my_string)
