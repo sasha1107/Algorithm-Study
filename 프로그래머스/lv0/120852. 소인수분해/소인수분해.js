0cd56bbff5a80eda33d8fd9a1bc63fc680dae9e6
정수현 <78977003+sasha1107@users.noreply.github.com>
2022-12-11 01:20:09 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = [];
+    let i = 2;
+    while (n !== 1){
+        if (n % i == 0){
+            answer.push(i);
+            n /= i;
+        }
+        else {
+            i++;
+        }
+    }
+    return [...new Set(answer)];
+}
