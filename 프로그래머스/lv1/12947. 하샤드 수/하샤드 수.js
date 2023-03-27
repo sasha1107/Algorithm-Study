aa52979e3c4a7a880a3d0aab2487cbded8486847
정수현 <78977003+sasha1107@users.noreply.github.com>
2023-03-27 14:32:34 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,3 +1,3 @@
 function solution(x) {
-    return x % x.toString().split('').reduce((a,c) => +a + +c) === 0 ? true : false;
+    return !!(x % x.toString().split('').reduce((a,c) => +a + +c) === 0)
 }
