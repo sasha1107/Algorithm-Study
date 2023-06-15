ecfa91cd50eebab52892a66fc5f94cfe567d793d
정수현 <78977003+sasha1107@users.noreply.github.com>
2023-06-16 00:23:35 +0900

    [unrated] Title: 달리기 경주, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,26 +1,25 @@
+function swapKeysAndValues(obj) {
+  const swapped = Object.entries(obj).map(
+    ([key, value]) => [value, key]
+  );
+  return Object.fromEntries(swapped);
+}
 function solution(players, callings) {
-    var answer = [];
-    const order = {}; 
-    // 	{ '0': 'mumu', '1': 'soe', '2': 'poe', '3': 'kai', '4': 'mine' } order
-    // 	{ '0': 'mumu', '1': 'soe', '2': 'kai', '3': 'poe', '4': 'mine' } order
-    const orderString = {}
-    // { mumu: 0, soe: 1, poe: 2, kai: 3, mine: 4 } orderString
-    // { mumu: 0, soe: 1, kai: 2, poe: 3, mine: 4 } orderString
+    const ranking = {}; 
     for (let i = 0; i < players.length; i++){
-        order[i] = players[i]
+        ranking[i] = players[i]
     }
-    for (let i = 0; i < players.length; i++){
-        orderString[players[i]] = i
-    }
+    const swappedRanking = swapKeysAndValues(ranking)
     for (let i = 0; i < callings.length; i++){
-        const 앞 = orderString[callings[i]] - 1 // 2
-        const 뒤 = orderString[callings[i]] // 3
-        const 당한자 = order[앞] // soe
-        order[앞] = callings[i]
-        order[앞 + 1] = 당한자
-        orderString[callings[i]] = 앞
-        orderString[당한자] = 뒤
+        const front = swappedRanking[callings[i]] - 1
+        const back = front + 1
+        const passed = ranking[front]
+        ranking[front] = callings[i]
+        ranking[back] = passed
+        swappedRanking[callings[i]] = front
+        swappedRanking[passed] = back
+        
     }
     
-    return Object.values(order);
+    return Object.values(ranking);
 }
