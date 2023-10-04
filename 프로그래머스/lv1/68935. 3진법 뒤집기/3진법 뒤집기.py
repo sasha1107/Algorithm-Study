871eb2e058c2fe8231f0e2fc345e4aa25087e8c7
정수현 <78977003+sasha1107@users.noreply.github.com>
2023-10-04 10:13:51 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -0,0 +1,13 @@
+import string
+
+tmp = string.digits+string.ascii_lowercase
+def convert(num, base) :
+    q, r = divmod(num, base)
+    if q == 0 :
+        return tmp[r] 
+    else :
+        return convert(q, base) + tmp[r]
+
+def solution(n):
+    reversed = convert(n, 3)[::-1]
+    return int(str(reversed), 3)
